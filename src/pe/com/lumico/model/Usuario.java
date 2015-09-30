2d1af087e1fe501f0ca66c9a444143f218be60aa
luis <luis@192.168.0.13>
2015-09-30 01:05:18 -0500

    Inicio del proyecto

---
+++ src/pe/com/lumico/model/Usuario.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pe.com.lumico.model;
+
+/**
+ *
+ * @author luis
+ */
+public class Usuario {
+    private Integer codigo;
+
+    public Usuario() {
+    }
+    
+    
+}
